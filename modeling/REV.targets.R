02725e7aa3881caae1207cfeed9157bd143f4135
Sam Hokin <shokin@ncgr.org>
2017-02-09 07:45:06 -0700

    Touch-up.

---
+++ modeling/REV.targets.R
@@ -0,0 +1,35 @@
+##
+## the list of known REV targets from Reinhart, et al.
+##
+
+REV.targets = c(
+    "AT5G43810",
+    "AT4G32980",
+    "AT2G44910",
+    "AT4G18700",
+    "AT1G13110",
+    "AT5G05690",
+    "AT1G26770",
+    "AT4G17460",
+    "AT5G47370",
+    "AT3G60390",
+    "AT4G16780",
+    "AT5G06710",
+    "AT4G37790",
+    "AT2G02080",
+    "AT2G02070",
+    "AT3G13810",
+    "AT3G23630",
+    "AT1G32240",
+    "AT4G10770",
+    "AT4G26950",
+    "AT2G40330",
+    "AT1G70560",
+    "AT3G54990",
+    "AT3G47620",
+    "AT2G41940",
+    "AT2G45450",
+    "AT3G60890",
+    "AT3G52770",
+    "AT2G36307"
+)
