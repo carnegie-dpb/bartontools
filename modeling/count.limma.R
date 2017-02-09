52cda056abd3b794d578398fb909dfeedb687142
Sam Hokin <shokin@ncgr.org>
2017-02-09 07:52:30 -0700

    Moved to standalone repo.

--- modeling/count.limma.R
+++
@@ -1,8 +0,0 @@
-## given an input data frame that has AT loci as row names, count the number that do and don't have significant limma time results for the given schema, condition
-
-count.limma = function(schema, condition, df) {
-
-
-
-  
-}
--- modeling/curvature.R
+++
@@ -1,7 +0,0 @@
-##
-## return the curvature of a quadratic fit to the given FPKM expression, normalized to FPKM(0)
-##
-curvature = function(times, values) {
-  c = lm(values ~ times + I(times^2))
-  print(paste("NormCurv=", as.numeric(c$coefficients[3]/mean(values[1:3])), "AdjR-Squared=", summary(c)$adj.r.squared))
-}
