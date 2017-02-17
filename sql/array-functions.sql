bd4bf2065a36289b81b2dc5702578417322941a2
Sam Hokin <shokin@ncgr.org>
2017-02-17 14:27:57 -0700

    Cleaned house, put all functions in array-functions.sql (including aggregates, because they use array functions).

--- sql/array-functions.sql
+++ sql/array-functions.sql
@@ -2,6 +2,17 @@
 -- Cook some array functions
 --
 
+CREATE OR REPLACE FUNCTION array_median(double precision[])
+   RETURNS double precision AS $$
+   SELECT avg(v) FROM (
+     SELECT v
+     FROM unnest($1) v
+     ORDER BY 1
+     LIMIT  2 - mod(array_upper($1, 1), 2)
+     OFFSET ceil(array_upper($1, 1) / 2.0) - 1
+     ) sub;
+     $$ LANGUAGE 'sql';
+ 
 CREATE OR REPLACE FUNCTION array_avg(double precision[])
        RETURNS double precision AS $$
        SELECT avg(v) FROM unnest($1) g(v)
@@ -10,11 +21,6 @@ CREATE OR REPLACE FUNCTION array_avg(double precision[])
 CREATE OR REPLACE FUNCTION array_variance(double precision[])
        RETURNS double precision AS $$
        SELECT variance(v) FROM unnest($1) g(v)
-       $$ LANGUAGE sql;
-
-CREATE OR REPLACE FUNCTION array_median(double precision[])
-       RETURNS double precision AS $$
-       SELECT median(v) FROM unnest($1) g(v)
        $$ LANGUAGE sql;
 
 CREATE OR REPLACE FUNCTION array_max(double precision[])
@@ -42,7 +48,9 @@ CREATE OR REPLACE FUNCTION array_mode(integer[])
        SELECT v FROM unnest($1) g(v) GROUP BY 1 ORDER BY COUNT(1) DESC, 1 LIMIT 1
        $$ LANGUAGE sql;
 
-
-
-
+CREATE AGGREGATE median(double precision) (
+  SFUNC=array_append,
+  STYPE=double precision[],
+  FINALFUNC=array_median,
+  INITCOND='{}');
 
