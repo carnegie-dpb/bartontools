283c205404106c2935fa4834edede9530b838321
Sam Hokin <shokin@ncgr.org>
2017-02-17 14:11:23 -0700

    Cleaning up.

--- sql/array-functions.sql
+++ sql/array-functions.sql
@@ -17,6 +17,32 @@ CREATE OR REPLACE FUNCTION array_median(double precision[])
        SELECT median(v) FROM unnest($1) g(v)
        $$ LANGUAGE sql;
 
+CREATE OR REPLACE FUNCTION array_max(double precision[])
+       RETURNS double precision AS $$
+       SELECT max(v) FROM unnest($1) g(v)
+       $$ LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION array_min(double precision[])
+       RETURNS double precision AS $$
+       SELECT min(v) FROM unnest($1) g(v)
+       $$ LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION array_range(double precision[])
+       RETURNS double precision AS $$
+       SELECT max(v)-min(v) FROM unnest($1) g(v)
+       $$ LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION array_mode(text[])
+       RETURNS text AS $$
+       SELECT v FROM unnest($1) g(v) GROUP BY 1 ORDER BY COUNT(1) DESC, 1 LIMIT 1
+       $$ LANGUAGE sql;
+
+CREATE OR REPLACE FUNCTION array_mode(integer[])
+       RETURNS integer AS $$
+       SELECT v FROM unnest($1) g(v) GROUP BY 1 ORDER BY COUNT(1) DESC, 1 LIMIT 1
+       $$ LANGUAGE sql;
+
+
 
 
 
