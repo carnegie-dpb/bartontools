1c524c5ca533a002f3eb3b27fcd22b4c72350451
Sam Hokin <shokin@carnegiescience.edu>
2018-04-23 16:57:58 +0000

    Updated how toolButtons are positions, per tool.

---
+++ sql/create-limmaresults.sql
@@ -0,0 +1,23 @@
+--
+-- perhaps a short-term fix; code wants schema.limmaresults for every experiment
+--
+
+CREATE TABLE "e-mtab-4316".limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE fowler1.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE grtiny.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse12715.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse22982.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse30702.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse34241.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse43858.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse5629.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse5630.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse5631.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse5632.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse5633.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse5634.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse57953.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse607.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse70100.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE gse70796.limmaresults () INHERITS (public.limmaresults);
+CREATE TABLE nasc592.limmaresults () INHERITS (public.limmaresults);
---
+++ sql/reassign-shokin.sql
@@ -0,0 +1,4 @@
+--
+-- Reassign stuff owned by shokin to sam
+--
+REASSIGN OWNED BY shokin TO sam;
